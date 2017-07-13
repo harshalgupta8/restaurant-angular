b1d009e6d990f447315637b2dae1f388ebb74522
harshalgupta8 <harshalgupta0804@gmail.com>
2017-07-14 03:00:10 +0530

    Update app.js

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,22 +1,25 @@
 'use strict';
-angular.module('confusionApp', []).controller('menuController', function() {
-  this.tab = 1;
-  this.filtText = '';
+angular.module('confusionApp', [])
+.controller('MenuController',['$scope', function($scope) {
   
-  var dishes = [
-    {
+  $scope.tab = 1;
+  $scope.filtText = '';
+  $scope.showDetails = false;
+
+  $scope.dishes = [
+   {
       name:'Uthapizza',
       image: 'images/uthapizza.png',
       category: 'mains',
       label:'Hot',
       price:'4.99',
-      description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
+      description:'An Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
       comment: ''
     },
     
     {
-      name:'Zucchipakoda',
-      image: 'images/zucchipakoda.png',
+      name:'Zucchida',
+      image: 'images/zucchihpakoda.png',
       category: 'appetizer',
       label:'',
       price:'1.99',
@@ -25,12 +28,12 @@ angular.module('confusionApp', []).controller('menuController', function() {
     },
     
     {
-      name:'Vadonut',
+      name:'Vada pao',
       image: 'images/vadonut.png',
       category: 'appetizer',
       label:'New',
       price:'1.99',
-      description:'A quintessential ConFusion experience, is it a vada or is it a donut?',
+      description:'Taste from the streets of Mumbai',
       comment: ''
     },
     
@@ -40,31 +43,63 @@ angular.module('confusionApp', []).controller('menuController', function() {
       category: 'dessert',
       label:'',
       price:'2.99',
-      description:'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms',
+      description:'A sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms',
       comment: ''
     }
   ]; 
   
-  this.dishes = dishes;
 
-  this.select = function(setTab) {
-    this.tab = setTab;
+  $scope.select = function(setTab) {
+    $scope.tab = setTab;
     
     if (setTab === 2) {
-      this.filtText = "appetizer";
+      $scope.filtText = "appetizer";
     } 
     else if (setTab === 3) {
-      this.filtText = "mains";
+      $scope.filtText = "mains";
     }
     else if (setTab === 4) {
-      this.filtText = "dessert";
+      $scope.filtText = "dessert";
     }
     else {
-      this.filtText = "";
+      $scope.filtText = "";
     }
   };
   
-  this.isSelected = function (checkTab) {
-    return (this.tab === checkTab);
+  $scope.isSelected = function (checkTab) {
+    return ($scope.tab === checkTab);
   };
-});
+
+  $scope.toggleDetails = function(){
+    $scope.showDetails = !$scope.showDetails;
+  };
+}])
+
+.controller('ContactController', ['$scope', function($scope) {
+
+  $scope.feedback = {mychannel:"", firstName:"", lastName:"",
+         agree:false, email:"" };
+
+  var channels = [{value:"tel", label:"Tel."}, {value:"Email",label:"Email"}];
+            $scope.channels = channels;
+            $scope.invalidChannelSelection = false;
+        }])
+
+.controller('FeedbackController', ['$scope', function($scope) {
+          $scope.sendFeedback = function() {
+                          console.log($scope.feedback);
+                          if ($scope.feedback.agree && ($scope.feedback.mychannel == "")) {
+                                   $scope.invalidChannelSelection = true;
+                    console.log('incorrect');
+                }
+                else {
+                    $scope.invalidChannelSelection = false;
+                    $scope.feedback = {mychannel:"", firstName:"", lastName:"",
+                                       agree:false, email:"" };
+                    $scope.feedback.mychannel="";
+
+                    $scope.feedbackForm.$setPristine();
+                    console.log($scope.feedback);
+                }
+            };
+        }]);
